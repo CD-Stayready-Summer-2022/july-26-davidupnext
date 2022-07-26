6131b183f78d015924c0a0c1f35de6380668d630
davidupnext <dadeleke31@gmail.com>
2022-07-26 09:28:08 -0400

    Done

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,32 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        String[] inputs = input.split("");
+
+        Map<String, Integer> wordCount = new TreeMap<>();
+
+        for (String in : inputs) {
+            if (!wordCount.containsKey(in))
+                wordCount.put(in, 1);
+            else {
+                int count = wordCount.get(in);
+                wordCount.put(in, count + 1);
+            }
+        }
+
+        Iterator<Map.Entry<String, Integer>> iterator = wordCount.entrySet().iterator();
+        StringBuilder builder = new StringBuilder();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            builder.append(entry.getKey() + ":" + entry.getValue()+ " ");
+        }
+
+        return builder.toString();
     }
 }
